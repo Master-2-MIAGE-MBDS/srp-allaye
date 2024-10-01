d1d79dda660041609564892317832187cbb28112
allaye35 <allaye35@gmail.com>
2024-10-01 12:23:08 +0200

    Ajout des fonctionnalités de gestion des tâches

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
-        taskManager.addTask("Tache 1", "Description de la tache 1");
-        taskManager.addTask("Tache 2", "Description de la tache 2");
-        taskManager.addTask("Tache 3", "Description de la tache 3");
 
-        taskManager.markTaskAsComplete(1);
+        taskManager.addTask("Tâche 1", "Description de la tâche 1");
+        taskManager.addTask("Tâche 2", "Description de la tâche 2");
+
+        taskManager.markTaskAsComplete(0); // Marque la première tâche comme terminée
 
         taskManager.printCompletedTasks();
         taskManager.printPendingTasks();
